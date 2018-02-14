617d12ad9f992ac7e858d22d1c50413651a207c6
Gerald Png <geraldpng@gmail.com>
2018-02-14 11:02:44 +0800

    major refactor of header into container vs component

--- src/containers/landing/header/Header.js
+++ src/containers/landing/header/Header.js
@@ -1,86 +1,40 @@
 // module imports
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import Button from 'material-ui/Button';
-import IconButton from 'material-ui/IconButton';
-import MenuIcon from 'material-ui-icons/Menu';
-import Hidden from 'material-ui/Hidden';
-import Menu, { MenuItem } from 'material-ui/Menu';
-import Dialog, {
-  DialogContent,
-  DialogTitle,
-  withMobileDialog
-} from 'material-ui/Dialog';
-import Close from 'material-ui-icons/Close';
-import FlexView from 'react-flexview';
 import { toastr } from 'react-redux-toastr';
-import { Link } from 'react-router-dom';
 
 // local imports
 import * as actions from 'actions';
 import sysParams from 'sys_params';
-import CompanySignupForm from 'components/forms/CompanySignupForm';
-import LoginForm from 'components/forms/LoginForm';
+import HeaderComponent from 'components/landing/header/Header';
 
 // style imports
-const styles = {
-  menuButton: {
-    marginRight: 8
-  }
-};
 
 class Header extends Component {
   constructor() {
     super();
     this.state = {
-      dialogOpen: false,
-      anchorEl: null,
-      form: null
+      toggle: true
     };
   }
 
-  handleDialogOpen = formName => {
-    this.setState({ dialogOpen: true, form: formName });
-  };
-
-  handleDialogClose = () => {
-    this.setState({ dialogOpen: false });
-  };
-
-  handleMenuOpen = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  handleMenuClose = () => {
-    this.setState({ anchorEl: null });
-  };
-
-  handleSubmit = async values => {
-    let res;
-    switch (this.state.form) {
-      case 'login':
-        res = await this.props.login(values);
-        break;
-      case 'signup':
-        res = await this.props.signup(values, sysParams.roles.company);
-        break;
-      default:
-        return { success: false };
-    }
+  handleLogin = async values => {
+    const res = await this.props.login(values);
     if (res.success) {
-      this.handleDialogClose();
-      switch (this.state.form) {
-        case 'login':
-          this.props.history.push('/dashboard');
-          return toastr.success('Login Successful', 'Welcome to Referix');
-        case 'signup':
-          return toastr.success('Signup Successful', 'Please log in');
-        default:
-          return null;
-      }
+      this.props.history.push('/dashboard');
+      return toastr.success('Login Successful', 'Welcome to Referix');
+    } else {
+      toastr.error('Authentication Failed', res.message);
+    }
+  };
+
+  handleSignup = async values => {
+    const res = await this.props.signup(values, sysParams.roles.company);
+    if (res.success) {
+      this.setState({
+        toggle: !this.state.toggle
+      });
+      return toastr.success('Signup Successful', 'Please log in');
     } else {
       toastr.error('Authentication Failed', res.message);
     }
@@ -91,177 +45,15 @@ class Header extends Component {
     this.handleDialogClose();
   };
 
-  renderDialogTitle = () => {
-    switch (this.state.form) {
-      case 'login':
-        return 'Log In';
-      case 'signup':
-        return 'Registration';
-      default:
-        return null;
-    }
-  };
-
-  renderDialogContent = () => {
-    switch (this.state.form) {
-      case 'login':
-        return <LoginForm onSubmit={this.handleSubmit} />;
-      case 'signup':
-        return <CompanySignupForm onSubmit={this.handleSubmit} />;
-      default:
-        return null;
-    }
-  };
-
-  renderHeaderToolbar = () => {
-    switch (this.props.authenticated) {
-      case true:
-        return [
-          <Link
-            to="/dashboard"
-            style={{ textDecoration: 'none', color: 'white' }}
-            key="toolbar-dashboard"
-          >
-            <Button color="inherit">Dashboard</Button>
-          </Link>,
-          <Button
-            color="inherit"
-            onClick={this.handleLogout}
-            key="toolbar-logout"
-          >
-            Logout
-          </Button>
-        ];
-      case false:
-        return (
-          <FlexView>
-            <Button
-              color="inherit"
-              onClick={this.handleDialogOpen.bind(this, 'signup')}
-            >
-              Sign Up
-            </Button>
-            <Button
-              color="inherit"
-              onClick={this.handleDialogOpen.bind(this, 'login')}
-            >
-              Login
-            </Button>
-          </FlexView>
-        );
-      default:
-        return null;
-    }
-  };
-
-  renderHeaderMenu = () => {
-    switch (this.props.authenticated) {
-      case true:
-        return (
-          <FlexView column>
-            <Link
-              to="/dashboard"
-              style={{ textDecoration: 'none', color: 'white' }}
-              key="toolbar-dashboard"
-            >
-              <MenuItem>Dashboard</MenuItem>
-            </Link>
-            <MenuItem onClick={this.handleMenuClose && this.handleLogout}>
-              Logout
-            </MenuItem>
-          </FlexView>
-        );
-      case false:
-        return (
-          <FlexView column>
-            <MenuItem
-              onClick={
-                this.handleMenuClose &&
-                this.handleDialogOpen.bind(this, 'login')
-              }
-            >
-              Login
-            </MenuItem>
-            <MenuItem
-              onClick={
-                this.handleMenuClose &&
-                this.handleDialogOpen.bind(this, 'signup')
-              }
-            >
-              Sign Up
-            </MenuItem>
-          </FlexView>
-        );
-      default:
-        return null;
-    }
-  };
-
   render() {
-    const { fullScreen } = this.props;
-    const { anchorEl } = this.state;
-    const openMenu = Boolean(anchorEl);
-
     return (
-      <FlexView>
-        <AppBar position="static">
-          <Toolbar>
-            <FlexView grow>
-              <Typography type="title" color="inherit">
-                Referix
-              </Typography>
-            </FlexView>
-            <Hidden xsDown>{this.renderHeaderToolbar()}</Hidden>
-            <Hidden smUp>
-              <IconButton
-                color="inherit"
-                aria-label="Menu"
-                style={styles.menuButton}
-                onClick={this.handleMenuOpen}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right'
-                }}
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right'
-                }}
-                open={openMenu}
-                onClick={this.handleMenuClose}
-              >
-                {this.renderHeaderMenu()}
-              </Menu>
-            </Hidden>
-          </Toolbar>
-        </AppBar>
-        <Dialog
-          fullScreen={fullScreen}
-          open={this.state.dialogOpen}
-          onClose={this.handleDialogClose}
-          aria-labelledby="responsive-dialog-title"
-        >
-          <DialogTitle style={{ minWidth: fullScreen ? 0 : 400 }}>
-            {this.renderDialogTitle()}
-            <Close
-              style={{
-                float: 'right',
-                cursor: 'pointer',
-                marginTop: '-10px',
-                marginRight: '-10px',
-                width: '20px'
-              }}
-              onClick={this.handleDialogClose}
-            />
-          </DialogTitle>
-          <DialogContent>{this.renderDialogContent()}</DialogContent>
-        </Dialog>
-      </FlexView>
+      <HeaderComponent
+        authenticated={this.props.authenticated}
+        handleLogin={this.handleLogin}
+        handleSignup={this.handleSignup}
+        handleLogout={this.handleLogout}
+        dialogClose={this.state.toggle}
+      />
     );
   }
 }
@@ -270,4 +62,4 @@ function mapStateToProps({ auth }) {
   return { authenticated: auth.authenticated };
 }
 
-export default withMobileDialog()(connect(mapStateToProps, actions)(Header));
+export default connect(mapStateToProps, actions)(Header);
